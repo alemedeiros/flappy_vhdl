b5e17b734b08613cabc0e5d9454597a830441428
Alexandre Medeiros <alexandre.n.medeiros@gmail.com>
2014-06-17 18:30:46 -0300

    Improve pixel_counter

--- output/pixel_counter.vhd
+++ output/pixel_counter.vhd
@@ -11,16 +11,12 @@ use ieee.std_logic_1164.all ;
 
 entity pixel_counter is
 	generic (
-				--  When changing this, remember to keep 4:3 aspect ratio
-				--  Must also keep in mind that our native resolution is 640x480, and
-				--  you can't cross these bounds (although you will seldom have enough
-				--  on-chip memory to instantiate this module with higher res).
-				NUM_HORZ_PIXELS : natural := 128 ;  -- Number of horizontal pixels
-				NUM_VERT_PIXELS : natural := 96     -- Number of vertical pixels
+				H_HES : natural := 128 ;  -- Horizontal Resolution
+				V_RES : natural := 96     -- Vertical Resolution
 			) ;
 	port (
-			 lin : out integer range 0 to NUM_VERT_PIXELS - 1 ;
-			 col : out integer range 0 to NUM_HORZ_PIXELS - 1 ;
+			 lin : out integer range 0 to V_RES - 1 ;
+			 col : out integer range 0 to H_RES - 1 ;
 
 			 clock  : in std_logic ;
 			 reset  : in std_logic ;
@@ -29,16 +25,17 @@ entity pixel_counter is
 end pixel_counter ;
 
 architecture behavior of pixel_counter is
-	signal my_lin : integer range 0 to NUM_VERT_PIXELS - 1 ;
-	signal my_col : integer range 0 to NUM_HORZ_PIXELS - 1 ;
+	signal my_lin : integer range 0 to V_RES - 1 ;
+	signal my_col : integer range 0 to H_RES - 1 ;
 begin
+	-- Columns counter
 	count_col: process (clock, reset)
 	begin
 		if reset = '1' then
 			my_col <= 0 ;
 		elsif rising_edge(clock) then
 			if enable = '1' then
-				if my_col = 127 then
+				if my_col = H_RES - 1 then
 					my_col <= 0 ;
 				else
 					my_col <= my_col + 1 ;
@@ -47,13 +44,14 @@ begin
 		end if ;
 	end process count_col ;
 
+	-- Lines counter
 	count_lin: process (clock, reset)
 	begin
 		if reset = '1' then
 			my_lin <= 0;
 		elsif rising_edge(clock) then
-			if enable = '1' and my_col = 127 then
-				if my_lin = 95 then
+			if enable = '1' and my_col = H_RES - 1 then
+				if my_lin = V_RES - 1 then
 					my_lin <= 0 ;
 				else
 					my_lin <= my_lin + 1 ;
