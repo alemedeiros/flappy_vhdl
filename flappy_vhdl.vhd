e57f6b193ff69cddd9a64bc06e1051ba49836dd7
Alexandre Medeiros <alexandre.n.medeiros@gmail.com>
2014-06-17 18:32:54 -0300

    Improve top-level debuging test for vga

--- flappy_vhdl.vhd
+++ flappy_vhdl.vhd
@@ -43,14 +43,22 @@ entity flappy_vhdl is
 end flappy_vhdl ;
 
 architecture behavior of flappy_vhdl is
+	signal timer : std_logic ;
 
+	signal pos  : integer range 31 downto 0;
+	signal play : integer range 0 to 95 ;
+
+	signal id   : integer range 0 to 3 ;
+	signal low  : integer range 0 to 95 ;
+	signal high : integer range 0 to 95 ;
 begin
 	output: draw_frame
 	port map (
-				 position   => "10101010",
-				 obst_low   => "00001111",
-				 obst_high  => "11110000",
-				 obst_id    => open,
+				 player     => play,
+				 obst_low   => low,
+				 obst_high  => high,
+				 obst_pos   => pos,
+				 obst_id    => id,
 				 red        => vga_r,
 				 green      => vga_g,
 				 blue       => vga_b,
@@ -58,11 +66,76 @@ begin
 				 vsync      => vga_vs,
 				 clock      => clock_27,
 				 enable     => '1',
-
-				 -- DEBUG
-				 hex        => hex0,
-				 st         => sw(0),
-
-				 reset      => key(1)
+				 reset      => not key(1)
 			 ) ;
+
+	-- Simple 1 second timer
+	process(clock_27)
+		variable count : integer range 0 to 2000000 := 0 ;
+	begin
+		if rising_edge(clock_27) then
+			count := count + 1 ;
+		end if ;
+		
+		if count = 0 then
+			timer <= '1' ;
+		else
+			timer <= '0' ;
+		end if ;
+	end process ;
+
+	-- Change obst_pos value
+	process(timer)
+		variable tmp : integer range 31 downto 0 := 31 ;
+	begin
+		if rising_edge(timer) then
+			tmp := tmp - 1 ;
+		end if ;
+
+		pos <= tmp ;
+	end process ;
+
+	-- Change player value
+	process(timer)
+		variable tmp : integer range 0 to 95 := 47 ;
+		variable dir : std_logic := '0' ;
+	begin
+		if rising_edge(timer) then
+			if dir = '0' then
+				tmp := tmp + 1 ;
+			else
+				tmp := tmp - 1 ;
+			end if ;
+		end if ;
+
+		if tmp = 30 then
+			dir := '0' ;
+		elsif tmp = 65 then
+			dir := '1' ;
+		end if ;
+
+		play <= tmp ;
+	end process ;
+
+	-- Changes obstacles values
+	process(id)
+	begin
+		case id is
+			when 0 =>
+				low <= 20 ;
+				high <= 20 ;
+			when 1 =>
+				low <= 40 ;
+				high <= 40 ;
+			when 2 =>
+				low <= 40 ;
+				high <= 30 ;
+			when 3 =>
+				low <= 30 ;
+				high <= 40 ;
+		end case ;
+	end process ;
+
+	ledg(3 downto 0) <= key ;
+	ledr <= sw ;
 end behavior ;
